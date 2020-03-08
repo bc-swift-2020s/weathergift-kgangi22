0125e80f466b3a38d210a6139bbd2293481853e9
Kyle Gangi <gangiky@bc.edu>
2020-03-08 12:18:28 -0400

    Section 7.2 Completed

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Kyle Gangi on 3/8/20.
+//  Copyright © 2020 Kyle Gangi. All rights reserved.
+//
+
+import Foundation
+
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
