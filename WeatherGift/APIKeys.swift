c20bdf24fff0ccbf6764f5dd614addd1225aca60
Kyle Gangi <gangiky@bc.edu>
2020-03-10 16:12:17 -0400

    with .gitignore

---
+++ WeatherGift/APIKeys.swift
@@ -0,0 +1,15 @@
+//
+//  APIKeys.swift
+//  WeatherGift
+//
+//  Created by Kyle Gangi on 3/10/20.
+//  Copyright © 2020 Kyle Gangi. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys{
+    
+    static let googlePlacesKey = "AIzaSyBOoM3Q7doNULBQorW8erWHXRDMaF1L8_c"
+    
+}
