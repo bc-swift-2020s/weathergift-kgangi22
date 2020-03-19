f158be4d297331aebb99e4294ff68c9af6008a78
Kyle Gangi <gangiky@bc.edu>
2020-03-19 13:05:02 -0400

    Section 7.10 Completed

--- WeatherGift/APIKeys.swift
+++ WeatherGift/APIKeys.swift
@@ -11,5 +11,13 @@ import Foundation
 struct APIkeys{
     
     static let googlePlacesKey = "AIzaSyBOoM3Q7doNULBQorW8erWHXRDMaF1L8_c"
+    static let darkSkyKey = "f53904a01380062739ab6fbfd18b383c"
     
 }
+
+struct APIurls{
+    
+    static let darkSkyURL = "https://api.darksky.net/forecast/"
+}
+
+
--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -19,4 +19,36 @@ class WeatherLocation: Codable {
         self.latitude = latitude
         self.longitude = longitude
     }
+    
+    func getData(){
+        let coordinates = "\(latitude),\(longitude)"
+        let urlString = "\(APIurls.darkSkyURL)\(APIkeys.darkSkyKey)/\(coordinates)"
+    
+        print("❤️❤️❤️❤️❤️❤️ We are accessing \(urlString)")
+        guard let url = URL(string: urlString) else{
+            print("ERROR: Could not create URL with \(urlString)")
+            return
+        }
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            }
+            do{
+                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print("\(json)")
+                
+            } catch{
+                print("JSON Error: \(error.localizedDescription)")
+                
+            }
+        }
+        
+        task.resume()
+        
+        
+        
+        
+        
+    }
 }
